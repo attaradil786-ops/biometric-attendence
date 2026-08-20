7fbed4481fde3a663cfd0a73b08e48e160386b03
adil attar <attaradil786@gmail.com>
2026-08-21 01:21:41 +0530

    Update db connection for cloud deployment

--- backend/db.js
+++ backend/db.js
@@ -1,20 +1,19 @@
+require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'attendance_system',
-  password: 'adilattar@07', // Replace with the password you set during installation
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.connect((err, client, release) => {
   if (err) {
-    return console.error('Connection error:', err.stack);
+    return console.error('Connection error', err.stack);
   }
-  console.log('Connected to PostgreSQL successfully!');
+  console.log('Connected to PostgreSQL database');
   release();
 });
 
 module.exports = pool;
-
