e6490448804a1dc0878fee96792f49d1182bab8f
adil attar <attaradil786@gmail.com>
2026-08-21 02:53:44 +0530

    Update db connection logic

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,15 @@
-require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false,
+  },
 });
 
 pool.connect((err, client, release) => {
   if (err) {
-    return console.error('Connection error', err.stack);
+    return console.error('Connection error:', err.message);
   }
   console.log('Connected to PostgreSQL database');
   release();
